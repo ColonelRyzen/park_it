7ff02998ff8bb9716183bd6ec425acb179f7f7a4
Zachary Salim <zjsalim96@gmail.com>
2019-12-03 23:55:28 -0500

    switching away from 2 databases

--- src/car_detection.py
+++ src/car_detection.py
@@ -17,21 +17,21 @@ def detect_car():
         if in_range_count == config.car_detection_threshold:
             car_detection = True
             doc_data = database_comms.get_document_data(config.spot_database_name, config.spot_num)
-            lot_data = database_comms.get_document_data(config.lot_database_name, config.lot_id)
+            # lot_data = database_comms.get_document_data(config.lot_database_name, config.lot_id)
             if doc_data["taken"] == False:
                 doc_data["taken"] = True
                 database_comms.set_document_data(config.spot_database_name, config.spot_num, doc_data)
-                lot_data["num_free_spots"] = lot_data["num_free_spots"] - 1
-                database_comms.set_document_data(config.lot_database_name, config.lot_id, lot_data)
+                # lot_data["num_free_spots"] = lot_data["num_free_spots"] - 1
+                # database_comms.set_document_data(config.lot_database_name, config.lot_id, lot_data)
         elif in_range_count == config.car_detection_threshold - config.car_stop_detecting_delay:
             car_detection = False
             doc_data = database.get_document_data(config.spot_database_name, config.spot_num)
-            lot_data = database_comms.get_document_data(config.lot_database_name, config.lot_id)
+            # lot_data = database_comms.get_document_data(config.lot_database_name, config.lot_id)
             if doc_data["taken"] == True:
                 doc_data["taken"] = False
                 database_comms.set_document_data(config.spot_database_name, config.spot_num, doc_data)
-                lot_data["num_free_spots"] = lot_data["num_free_spots"] + 1
-                database_comms.set_document_data(config.lot_database_name, config.lot_id, lot_data)
+                # lot_data["num_free_spots"] = lot_data["num_free_spots"] + 1
+                # database_comms.set_document_data(config.lot_database_name, config.lot_id, lot_data)
         if in_range_bool == True and in_range_count < config.car_detection_threshold:
             in_range_count += 1
         elif in_range_count == False:
--- src/seven_segment_display.py
+++ src/seven_segment_display.py
@@ -1,34 +1,35 @@
 import config
-import seven_segment_ctrl
+# import seven_segment_ctrl
 import database_comms
 import time
 
 def update_sev_seg():
     sev_seg_value = config.max_num_spots
     while True:
-        lot_data = database_comms.get_document_data(config.lot_database_name, config.lot_id)
+        spot_data = database_comms.get_document_data(config.spot_database_name, config.lot_id)
         time.sleep(2)
 
+        print(lot_data)
+
+        print("num_free_spots: ", spot_data["num_free_spots"])
         num_free_spots = lot_data["num_free_spots"]
-        print("num_free_spots: ", num_free_spots)
-        if num_free_spots == 0:
+        if num_free_spots = 0:
             seven_segment.zero()
-        elif num_free_spots == 1:
+        elif num_free_spots = 1:
             seven_segment.one()
-        elif num_free_spots == 2:
+        elif num_free_spots = 2:
             seven_segment.two()
-        elif num_free_spots == 3:
+        elif num_free_spots = 3:
             seven_segment.three()
-        elif num_free_spots == 4:
+        elif num_free_spots = 4:
             seven_segment.four()
-        elif num_free_spots == 5:
+        elif num_free_spots = 5:
             seven_segment.five()
-        elif num_free_spots == 6:
+        elif num_free_spots = 6:
             seven_segment.six()
-        elif num_free_spots == 7:
+        elif num_free_spots = 7:
             seven_segment.seven()
-        elif num_free_spots == 8:
+        elif num_free_spots = 8:
             seven_segment.eight()
-        elif num_free_spots == 9:
+        elif num_free_spots = 9:
             seven_segment.nine()
-            
