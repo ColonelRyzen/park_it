85882f28fc52bdb732b3637ce7a05d571b63973c
Zachary Salim <zjsalim96@gmail.com>
2019-12-04 00:59:44 -0500

    Added main functions back

--- src/seven_segment_ctrl.py
+++ src/seven_segment_ctrl.py
@@ -139,69 +139,69 @@ def nine():
 	fOn()
 	cOn()
 
-# def main():
-# 	while True:
-# 		cmd = input("-->")
-#
-# 		if cmd == "a on":
-# 			aOn()
-# 		elif cmd == "a off":
-# 			aOff()
-# 		elif cmd == "b on":
-# 			bOn()
-# 		elif cmd == "b off":
-# 			bOff()
-# 		elif cmd == "c on":
-# 			cOn()
-# 		elif cmd == "c off":
-# 			cOff()
-# 		elif cmd == "d on":
-# 			dOn()
-# 		elif cmd == "d off":
-# 			dOff()
-# 		elif cmd == "e on":
-# 			eOn()
-# 		elif cmd == "e off":
-# 			eOff()
-# 		elif cmd == "f on":
-# 			fOn()
-# 		elif cmd == "f off":
-# 			fOff()
-# 		elif cmd == "g on":
-# 			gOn()
-# 		elif cmd == "g off":
-# 			gOff()
-# 		elif cmd == "zero":
-# 			zero()
-# 		elif cmd == "one":
-# 			zero()
-# 			one()
-# 		elif cmd == "two":
-# 			zero()
-# 			two()
-# 		elif cmd == "three":
-# 			zero()
-# 			three()
-# 		elif cmd == "four":
-# 			zero()
-# 			four()
-# 		elif cmd == "five":
-# 			zero()
-# 			five()
-# 		elif cmd == "six":
-# 			zero()
-# 			six()
-# 		elif cmd == "seven":
-# 			zero()
-# 			seven()
-# 		elif cmd == "eight":
-# 			zero()
-# 			eight()
-# 		elif cmd == "nine":
-# 			zero()
-# 			nine()
-# 		else:
-# 			print("Not a valid input")
-#
-# 	return
-# main()
+def main():
+	while True:
+		cmd = input("-->")
+
+		if cmd == "a on":
+			aOn()
+		elif cmd == "a off":
+			aOff()
+		elif cmd == "b on":
+			bOn()
+		elif cmd == "b off":
+			bOff()
+		elif cmd == "c on":
+			cOn()
+		elif cmd == "c off":
+			cOff()
+		elif cmd == "d on":
+			dOn()
+		elif cmd == "d off":
+			dOff()
+		elif cmd == "e on":
+			eOn()
+		elif cmd == "e off":
+			eOff()
+		elif cmd == "f on":
+			fOn()
+		elif cmd == "f off":
+			fOff()
+		elif cmd == "g on":
+			gOn()
+		elif cmd == "g off":
+			gOff()
+		elif cmd == "zero":
+			zero()
+		elif cmd == "one":
+			zero()
+			one()
+		elif cmd == "two":
+			zero()
+			two()
+		elif cmd == "three":
+			zero()
+			three()
+		elif cmd == "four":
+			zero()
+			four()
+		elif cmd == "five":
+			zero()
+			five()
+		elif cmd == "six":
+			zero()
+			six()
+		elif cmd == "seven":
+			zero()
+			seven()
+		elif cmd == "eight":
+			zero()
+			eight()
+		elif cmd == "nine":
+			zero()
+			nine()
+		else:
+			print("Not a valid input")
+
+	return
+main()
